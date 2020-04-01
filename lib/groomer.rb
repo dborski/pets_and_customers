87a35500160ca733442780f2094ccf26f2bfcdab
Derek Borski <derek.borski@gmail.com>
2020-04-01 16:51:26 -0600

    Adds customers_with_outstanding_balance method

--- lib/groomer.rb
+++ lib/groomer.rb
@@ -1,7 +1,27 @@
 class Groomer
 
-  attr_reader :name
+  attr_reader :name, :customers
   def initialize(name)
     @name = name
+    @customers = []
+  end
+
+  def new_customer(customer)
+    @customers << customer
+  end
+
+  def customers_with_oustanding_balance
+    @customers.find_all {|customer| customer.outstanding_balance > 0}
   end
 end
+
+# ## Iteration 3
+#
+# You have been contracted to build a software application for a pet grooming company. This company has many grooming centers and would like to be able to track data on each one of them. Specifically, they would like you to implement the following features:
+#
+# * Each Groomer has a unique name, as well as a way to read that data.
+# * Each Groomer has multiple customers, and each of those customers can have multiple pets.
+# * A Groomer can find all the customers with outstanding balances.
+# * A Groomer can count the number of pets of a given type. For example the groomer can count the number of cats, the number of dogs, etc. This should work for any pet type.
+#
+# Build upon your code from the first two iterations to complete this task.
--- test/groomer_test.rb
+++ test/groomer_test.rb
@@ -6,6 +6,17 @@ require './lib/groomer'
 
 class GroomerTest < Minitest::Test
 
+  def test_instance_stash
+    skip
+    # groomer1 = Groomer.new("Lloyd Christmas")
+    # joel = Customer.new("Joel", 2)
+    # frank = Customer.new("Frank", 3)
+    # elanore = Customer.new("Elanore", 4)
+    # samson = Pet.new({name: "Samson", type: :dog, age: 3})
+    # lucy = Pet.new({name: "Lucy", type: :cat, age: 12})
+    # chester = Pet.new({name: "Chester", type: :snake, age: 2})
+  end
+
   def test_it_exists
     groomer1 = Groomer.new("Lloyd Christmas")
 
@@ -16,5 +27,35 @@ class GroomerTest < Minitest::Test
     groomer1 = Groomer.new("Lloyd Christmas")
 
     assert_equal "Lloyd Christmas", groomer1.name
+  end
+
+  def test_can_have_customers
+    groomer1 = Groomer.new("Lloyd Christmas")
+    joel = Customer.new("Joel", 2)
+    frank = Customer.new("Frank", 3)
+
+    assert_equal [], groomer1.customers
+
+    groomer1.new_customer(joel)
+    assert_equal [joel], groomer1.customers
+
+    groomer1.new_customer(frank)
+    assert_equal [joel, frank], groomer1.customers
+  end
+
+  def test_check_for_outstanding_balances
+    groomer1 = Groomer.new("Lloyd Christmas")
+    joel = Customer.new("Joel", 2)
+    frank = Customer.new("Frank", 3)
+    elanore = Customer.new("Elanore", 4)
+
+    groomer1.new_customer(joel)
+    groomer1.new_customer(frank)
+    groomer1.new_customer(elanore)
+
+    joel.charge(5)
+    elanore.charge(15)
+
+    assert_equal [joel, elanore], groomer1.customers_with_oustanding_balance
   end
 end
